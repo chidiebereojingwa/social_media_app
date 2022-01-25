cc6cf90cb342339bd8e3309a96ff14cae7e08df4
chidiebereojingwa <ojingwaoji@gmail.com>
2022-01-25 22:20:20 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import { ReactDOM } from "react";
 
 function App() {
   const [language, setlanguage] = React.useState('Python');
+  const [yearsExperience, setyearsExperience] = React.useState(0);
 
   function handleChangeLanguage (){    
     setlanguage("Javascript")
   }
 
+  function handleChangeExperience (event){    
+    setyearsExperience(event.target.value)
+  }
+
   return (
     <div>
+      
       <button onClick={handleChangeLanguage}>Change language</button>
+      <div>
+      <input type="number" onChange={handleChangeExperience} placeholder="type years of experience"/>
+      </div>      
       <p>I am learnig {language}</p>
+      <p>{yearsExperience}</p>
     </div>
   );
 }
