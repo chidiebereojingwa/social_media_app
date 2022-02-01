45de3314d69546f5c149bcc667fe6fa95ea273b4
chidiebereojingwa <ojingwaoji@gmail.com>
2022-02-01 13:12:18 +0100

    Started Social-app

--- src/App.js
+++ src/App.js
@@ -1,124 +1,134 @@
 
 import React from 'react';
- 
 
 
-// const endpoint = "https://github.com/users/codeartistryio"
-const endpoint = "https://api.coindesk.com/v1/bpi/currentprice.json"
+
 
 export default function App() {
- const [coin, setCoin] = React.useState(null);
-  // React.useEffect(() => {
-  //   fetch(endpoint)
-  //     .then(res => res.json())
-  //     .then(data => setCoin(data))
-  // }, [])
-
-  // React.useEffect(async () => {
-  //   const res = await fetch(endpoint)
-  //    const data = await res.json();
-  //    setCoin(data)
-  // }, [])
-
-  React.useEffect(() => {
-    async function getCoin(){
-      const res = await fetch(endpoint)
-       const data = await res.json();
-       setCoin(data)
-    }
-    getCoin();
-  }, [])
-
-  
-
-  console.log(coin)
-
-  return coin ? (
-   <div>
-      <p>{coin.disclaimer}</p>
-      {/* <p>{coin.bpi[0]}</p> 
-      <p>{coin.bpi[0].rate}</p> */}
-    </div>
-      ) : <h1>Loading ....</h1>
-    
+  return <div>Hello</div>;
 }
 
 
 
 
-//Using useEffect to track mouse movement
+// first lesson
+
+// // const endpoint = "https://github.com/users/codeartistryio"
+// const endpoint = "https://api.coindesk.com/v1/bpi/currentprice.json"
 
 // export default function App() {
-//   const [mousePosition, setMousePosition] = React.useState({x:0, y:0});
+//  const [coin, setCoin] = React.useState(null);
+//   // React.useEffect(() => {
+//   //   fetch(endpoint)
+//   //     .then(res => res.json())
+//   //     .then(data => setCoin(data))
+//   // }, [])
+
+//   // React.useEffect(async () => {
+//   //   const res = await fetch(endpoint)
+//   //    const data = await res.json();
+//   //    setCoin(data)
+//   // }, [])
 
 //   React.useEffect(() => {
-    
-//       document.addEventListener('mousemove', handleMouseMove)
-//     }, []) 
+//     async function getCoin(){
+//       const res = await fetch(endpoint)
+//        const data = await res.json();
+//        setCoin(data)
+//     }
+//     getCoin();
+//   }, [])
 
-//   function handleMouseMove(event) {
-//     setMousePosition({ x: event.pageX, y: event.pageY})
-//   }
+  
 
-//   return (
-//   <div>
-//       <p> X: {mousePosition.x}</p>
-//       <p> Y: {mousePosition.y}</p>
-//   </div>
-//   )
-// }
+//   console.log(coin)
 
-
-
-// first deep with useEffect and useState
-
-// function App() {
-//   // const [language, setlanguage] = React.useState('Python');
-//   // const [yearsExperience, setyearsExperience] = React.useState(0);
-
-//   const [developer, setdeveloper] = React.useState({
-//     language: "Python",
-//     yearsExperience: 0,
-//     isEmployed: false
-//   });
-
-//   React.useEffect(() => {
-//     document.title = "Chidiebere"
-//   })
-
-//   function handleChangeLanguage (){    
-//     setdeveloper({
-//       language: "JavaScript",
-//       yearsExperience: 0
-//     })
-//   }
-
-//   function handleChangeExperience (event){    
-//     setdeveloper({
-//       ...developer,
-//       yearsExperience: event.target.value
-//     })
-//   }
-
-//   function toggleEmployment (event){    
-//     setdeveloper((prevState)=> ({
-//       ...prevState,
-//       isEmployed: !prevState.isEmployed
-//     }))
-//   }
-
-//   return (
-//     <div>
-//       <button onClick={toggleEmployment}>Toggle Employement</button>
-//       <button onClick={handleChangeLanguage}>Change language</button>
-//       <div>
-//       <input type="number" onChange={handleChangeExperience} placeholder="type years of experience"/>
-//       </div>      
-//       <p>I am learnig {developer.language}</p>
-//       <p>I have {developer.yearsExperience} years of experience</p>
-//       <p>I am {developer.isEmployed ? "Employed" : "UnEmployed"}</p>
+//   return coin ? (
+//    <div>
+//       <p>{coin.disclaimer}</p>
+//       {/* <p>{coin.bpi[0]}</p> 
+//       <p>{coin.bpi[0].rate}</p> */}
 //     </div>
-//   );
+//       ) : <h1>Loading ....</h1>
+    
 // }
 
-// export default App;
+
+
+
+// //Using useEffect to track mouse movement
+
+// // export default function App() {
+// //   const [mousePosition, setMousePosition] = React.useState({x:0, y:0});
+
+// //   React.useEffect(() => {
+    
+// //       document.addEventListener('mousemove', handleMouseMove)
+// //     }, []) 
+
+// //   function handleMouseMove(event) {
+// //     setMousePosition({ x: event.pageX, y: event.pageY})
+// //   }
+
+// //   return (
+// //   <div>
+// //       <p> X: {mousePosition.x}</p>
+// //       <p> Y: {mousePosition.y}</p>
+// //   </div>
+// //   )
+// // }
+
+
+
+// // first deep with useEffect and useState
+
+// // function App() {
+// //   // const [language, setlanguage] = React.useState('Python');
+// //   // const [yearsExperience, setyearsExperience] = React.useState(0);
+
+// //   const [developer, setdeveloper] = React.useState({
+// //     language: "Python",
+// //     yearsExperience: 0,
+// //     isEmployed: false
+// //   });
+
+// //   React.useEffect(() => {
+// //     document.title = "Chidiebere"
+// //   })
+
+// //   function handleChangeLanguage (){    
+// //     setdeveloper({
+// //       language: "JavaScript",
+// //       yearsExperience: 0
+// //     })
+// //   }
+
+// //   function handleChangeExperience (event){    
+// //     setdeveloper({
+// //       ...developer,
+// //       yearsExperience: event.target.value
+// //     })
+// //   }
+
+// //   function toggleEmployment (event){    
+// //     setdeveloper((prevState)=> ({
+// //       ...prevState,
+// //       isEmployed: !prevState.isEmployed
+// //     }))
+// //   }
+
+// //   return (
+// //     <div>
+// //       <button onClick={toggleEmployment}>Toggle Employement</button>
+// //       <button onClick={handleChangeLanguage}>Change language</button>
+// //       <div>
+// //       <input type="number" onChange={handleChangeExperience} placeholder="type years of experience"/>
+// //       </div>      
+// //       <p>I am learnig {developer.language}</p>
+// //       <p>I have {developer.yearsExperience} years of experience</p>
+// //       <p>I am {developer.isEmployed ? "Employed" : "UnEmployed"}</p>
+// //     </div>
+// //   );
+// // }
+
+// // export default App;
