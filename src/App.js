d9fc5d135f66cb48b46b608a6607e6e66bdcc9ec
chidiebereojingwa <ojingwaoji@gmail.com>
2022-02-02 01:10:07 +0100

    Started Social-app

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React from 'react';
 import Login from './components/Login';
 
 
+function App() {
+    const [user, setUser] = React.useState("");
 
+    console.log(user)
 
-export default function App() {
-    let user = ""
-
-    if(!user){
-        return <Login/>
+    
+    if (!user){
+        return <Login setUser={setUser}/>
     }
 
-  return <div>Hello</div>;
+
+  return <div>App</div>;
 }
+
+export default App
 
 
 // first lesson
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,19 @@
 import React from 'react';
 
-export default function Login() {
-  return <div>Login</div>;
+export default function Login({setUser}) {
+    const [username, setUsername] = React.useState("");
+
+    function handleSubmit(event){        
+        setUser(username)
+    }
+
+  return (
+  <div>
+     <h2>Please Login</h2>
+    <form onSubmit={handleSubmit}>
+        <input onChange={event => setUsername(event.target.value)} placeholder='input username' />
+        <button type='submit'>Submit</button>
+    </form>
+   </div>
+      );
 }
