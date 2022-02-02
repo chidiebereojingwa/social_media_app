4c18b3c9b52215e1de59bcc00c1a486b608a5f46
chidiebereojingwa <ojingwaoji@gmail.com>
2022-02-02 02:00:59 +0100

    Add createpost

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 function CreatePost() {
+    const [content, setContent] = React.useEffect("")
+    const [image, setImage] = React.useEffect(null)
   return (
   <div>
       <h2>Please Create your post</h2>
@@ -8,6 +10,7 @@ function CreatePost() {
           <input
           type="text"
           placeholder='write your post'
+          onChange={(e)=> setContent(e.target.value)}
           />
           <input 
           type="file"
