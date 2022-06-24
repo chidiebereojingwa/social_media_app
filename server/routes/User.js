79bd843a5f58c4a3dbaba2c46802cb0c112b6cc2
chidiebereojingwa <ojingwaoji@gmail.com>
2022-06-24 01:59:35 +0100

    login

--- server/routes/User.js
+++ server/routes/User.js
@@ -16,4 +16,31 @@ router.post("/register", (req, res) => {
   );
 });
 
+router.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  db.query(
+    "SELECT * FROM Users WHERE username = ?",
+    username,
+    (err, results) => {
+      if (err) {
+        console.log(err);
+      }
+      if (results.length > 0) {
+        if (password == results[0].password) {
+          res.json({ loggedIn: true, username: username });
+        } else {
+          res.json({
+            loggedIn: false,
+            message: "Wrong username/password combo!",
+          });
+        }
+      } else {
+        res.json({ loggedIn: false, message: "User doesn't exist" });
+      }
+    }
+  );
+});
+
 module.exports = router;
